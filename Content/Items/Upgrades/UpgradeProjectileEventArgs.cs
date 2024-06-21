c8ff2a9d38759bcc116d025239ad2ee1bb2847e9
scottsimply <53203532+scottysimply@users.noreply.github.com>
2024-06-21 10:44:35 -0500

    Created event args. Unused

---
+++ Content/Items/Upgrades/UpgradeProjectileEventArgs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Terraria;
+using Terraria.DataStructures;
+
+namespace deeprockitems.Content.Items.Upgrades
+{
+    public class UpgradeProjectileEventArgs : EventArgs
+    {
+        public int TimeLeft { get; set; }
+        public int[] Upgrades { get; set; }
+        public bool? ShouldContinue { get; set; } = null;
+    }
+}
