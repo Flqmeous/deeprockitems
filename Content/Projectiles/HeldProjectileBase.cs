3101d3bf1216052a7a8a283cb2776d9f1b8d097f
scottsimply <53203532+scottysimply@users.noreply.github.com>
2024-06-24 12:32:21 -0500

    Projectiles spawned as a held projecitle are now _slightly_ more accurate to vanilla code. Previously, they were off by 1 pixel

--- Content/Projectiles/HeldProjectileBase.cs
+++ Content/Projectiles/HeldProjectileBase.cs
@@ -173,6 +173,9 @@ public abstract class HeldProjectileBase : ModProjectile
                 Vector2 adjusted_speed = velocity.RotatedByRandom(Spread);
 
                 Projectile proj = Projectile.NewProjectileDirect(projectileOwner.GetSource_ItemUse_WithPotentialAmmo(sourceItem, ammoUsed), projectileOwner.Center, adjusted_speed, ProjectileToSpawn, Projectile.damage, Projectile.knockBack, projectileOwner.whoAmI, ai0: 0f, ai1: timeLeft, ai2: Projectile.ai[2]);
+                // Make sure projectile is _right_ on the center
+                proj.Center = projectileOwner.Center;
+
 
                 // Make sure the projectile goes the right direction after charging
 
