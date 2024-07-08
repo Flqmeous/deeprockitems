58530a4223d837b1d17f45a4380a0afe3f72d93b
scottsimply <scottistriggered@gmail.com>
2024-07-08 15:03:32 -0500

    Added new electric-related debuff

---
+++ Content/Buffs/ElectrifiedEnemy.cs
@@ -0,0 +1,33 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace deeprockitems.Content.Buffs
+{
+    public class ElectrifiedEnemy : ModBuff
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.debuff[Type] = true;
+        }
+        public override void Update(NPC npc, ref int buffIndex)
+        {
+            npc.GetGlobalNPC<ElectrifiedEnemyNPC>().IsElectrified = true;
+        }
+    }
+    public class ElectrifiedEnemyNPC : GlobalNPC
+    {
+        public bool IsElectrified { get; set; }
+        public override bool InstancePerEntity => true;
+        public override void ResetEffects(NPC npc)
+        {
+            IsElectrified = false;
+        }
+        public override void UpdateLifeRegen(NPC npc, ref int damage)
+        {
+            if (IsElectrified)
+            {
+                npc.lifeRegen -= 20; // 10 dps
+            }
+        }
+    }
+}
