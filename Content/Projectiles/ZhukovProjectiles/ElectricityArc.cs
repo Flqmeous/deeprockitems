3e5694a0d3343a7ade2816f8cedf2b557e659d2d
scottsimply <53203532+scottysimply@users.noreply.github.com>
2024-07-13 13:00:46 -0500

    Electricity arc projectile is unused now

--- Content/Projectiles/ZhukovProjectiles/ElectricityArc.cs
+++ Content/Projectiles/ZhukovProjectiles/ElectricityArc.cs
@@ -3,6 +3,8 @@ using Terraria.ModLoader;
 using Microsoft.Xna.Framework;
 using deeprockitems.Assets.Textures;
 using Microsoft.Xna.Framework.Graphics;
+using Terraria.ID;
+using deeprockitems.Content.Buffs;
 
 namespace deeprockitems.Content.Projectiles.ZhukovProjectiles
 {
@@ -13,40 +15,47 @@ namespace deeprockitems.Content.Projectiles.ZhukovProjectiles
             Projectile.height = 16;
             Projectile.width = 16;
             DynamicScale = new(1, 1);
-            Projectile.timeLeft = 60;
+            Projectile.timeLeft = 120;
             Projectile.friendly = true;
             Projectile.damage = 24;
             Projectile.penetrate = -1;
             Projectile.usesIDStaticNPCImmunity = true;
             Projectile.idStaticNPCHitCooldown = 10;
+            Projectile.tileCollide = false;
         }
         public override void SetStaticDefaults()
         {
             frameHeight = 16;
             frameWidth = 48;
         }
+        private int _timer = 0;
         public override void AI()
         {
+            // Add time
+            _timer++;
+
             // Random frame
             if (Projectile.timeLeft % 3 == 0)
             {
                 Projectile.frame = Main.rand.Next(0, 3);
+                // Spawn dust
+                Dust.NewDust(Projectile.Center, 8, 8, DustID.Electric, Scale: 0.5f);
             }
-            // Make X scale inversly proportional to timeleft
-            int minScale = 0;
+            // Make X scale inversly proportional to timer
+            DynamicScale.X = _timer / 22.5f;
         }
         private static int frameHeight;
         private static int frameWidth;
-        public Vector2 DynamicScale { get; set; }
+        public Vector2 DynamicScale;
         private Rectangle GetFrame => new Rectangle(0, Projectile.frame * frameHeight, frameWidth, frameHeight);
         public override bool PreDraw(ref Color lightColor)
         {
-            Main.EntitySpriteDraw(DRGTextures.ElectricityArc, Projectile.position - Main.screenPosition, GetFrame, Color.White, Projectile.rotation, new Vector2(frameWidth / 2, frameHeight / 2), DynamicScale, SpriteEffects.None);
+            Main.EntitySpriteDraw(DRGTextures.ElectricityArc, Projectile.position - Main.screenPosition, GetFrame, Color.White, Projectile.rotation, new Vector2(frameWidth / 2f, frameHeight / 2f), DynamicScale, SpriteEffects.None);
             return false;
         }
         public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
         {
-            base.OnHitNPC(target, hit, damageDone);
+            target.AddBuff(ModContent.BuffType<ElectrifiedEnemy>(), 300);
         }
     }
 }
