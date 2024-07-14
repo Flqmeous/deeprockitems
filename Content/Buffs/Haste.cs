192f73c3825de4621ef976d1dee28dc3b658dd50
scottsimply <scottistriggered@gmail.com>
2024-07-14 08:50:52 -0500

    Added Haste buff, which acts as a stronger effect to the swiftness potion.

---
+++ Content/Buffs/Haste.cs
@@ -0,0 +1,28 @@
+﻿using Terraria.ModLoader;
+using Terraria;
+
+namespace deeprockitems.Content.Buffs
+{
+    public class Haste : ModBuff
+    {
+        public override void Update(Player player, ref int buffIndex)
+        {
+            player.GetModPlayer<HastePlayer>().HasHaste = true;
+        }
+    }
+    public class HastePlayer : ModPlayer
+    {
+        public bool HasHaste { get; set; } = false;
+        public override void ResetEffects()
+        {
+            HasHaste = false;
+        }
+        public override void PreUpdateMovement()
+        {
+            if (HasHaste)
+            {
+                Player.moveSpeed *= 1.5f;
+            }   
+        }
+    }
+}
