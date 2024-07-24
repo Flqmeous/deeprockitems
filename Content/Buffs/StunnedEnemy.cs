796f85a140ce204fac9f4d437f4b0363e017c121
scottsimply <scottistriggered@gmail.com>
2024-07-24 16:02:23 -0500

    Added a stun debuff. Used by the plasma pistol for now. Unlike freeze, the enemy will continue whatever momentum and cancel AI.

---
+++ Content/Buffs/StunnedEnemy.cs
@@ -0,0 +1,38 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace deeprockitems.Content.Buffs
+{
+    public class StunnedEnemy : ModBuff
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.debuff[Type] = true;
+        }
+        public override void Update(NPC npc, ref int buffIndex)
+        {
+            npc.GetGlobalNPC<StunnedEnemyNPC>().IsStunned = true;
+        }
+    }
+    public class StunnedEnemyNPC : GlobalNPC
+    {
+        public bool IsStunned { get; set; } = false;
+        public override bool InstancePerEntity => true;
+        public override void SetDefaults(NPC entity)
+        {
+            entity.buffImmune[ModContent.BuffType<StunnedEnemy>()] = entity.boss;
+        }
+        public override void ResetEffects(NPC npc)
+        {
+            IsStunned = false;
+        }
+        public override bool PreAI(NPC npc)
+        {
+            if (IsStunned)
+            {
+                return false;
+            }
+            return base.PreAI(npc);
+        }
+    }
+}
