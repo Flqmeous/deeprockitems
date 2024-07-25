759887d640a3869d4aa374e4d765b7d9aa474553
scottsimply <53203532+scottysimply@users.noreply.github.com>
2024-07-25 15:41:32 -0500

    Stun now properly works on worm enemies, and has a visual effect attached to it.

--- Content/Buffs/StunnedEnemy.cs
+++ Content/Buffs/StunnedEnemy.cs
@@ -1,5 +1,10 @@
-﻿using Terraria;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using deeprockitems.Assets.Textures;
 using Terraria.ModLoader;
+using Terraria.DataStructures;
 
 namespace deeprockitems.Content.Buffs
 {
@@ -11,12 +16,24 @@ namespace deeprockitems.Content.Buffs
         }
         public override void Update(NPC npc, ref int buffIndex)
         {
+            // If we hit an npc with realLife, remove buff from here and apply to the parent
+            if (npc.realLife > -1 && npc.realLife != npc.whoAmI)
+            {
+                if (Main.netMode != NetmodeID.MultiplayerClient)
+                {
+                    Main.npc[npc.realLife].AddBuff(Type, npc.buffTime[buffIndex]);
+                    npc.DelBuff(buffIndex);
+                }
+                Main.npc[npc.realLife].GetGlobalNPC<StunnedEnemyNPC>().IsStunned = true;
+                return;
+            }
             npc.GetGlobalNPC<StunnedEnemyNPC>().IsStunned = true;
         }
     }
     public class StunnedEnemyNPC : GlobalNPC
     {
         public bool IsStunned { get; set; } = false;
+        private bool _oldStun = false;
         public override bool InstancePerEntity => true;
         public override void SetDefaults(NPC entity)
         {
@@ -28,11 +45,65 @@ namespace deeprockitems.Content.Buffs
         }
         public override bool PreAI(NPC npc)
         {
+            // If NPC was just stunned:
+            if (IsStunned && !_oldStun)
+            {
+                npc.velocity = new(0, 0);
+                // Share stun to the head of the npc.
+
+            }
+            _oldStun = IsStunned;
+
+            // If npc is still stunned
             if (IsStunned)
             {
+                if (!npc.noGravity)
+                {
+                    npc.velocity.Y += npc.gravity;
+                }
                 return false;
             }
             return base.PreAI(npc);
+        }
+        public override bool CanHitNPC(NPC npc, NPC target)
+        {
+            if (IsStunned) return false;
+            return base.CanHitNPC(npc, target);
+        }
+        public override bool CanHitPlayer(NPC npc, Player target, ref int cooldownSlot)
+        {
+            if (IsStunned) return false;
+            return base.CanHitPlayer(npc, target, ref cooldownSlot);
+        }
+        private int _stunFrameTimer = 0;
+        private int _stunFrame = 0;
+        private const int _frameCount = 4;
+        public override bool PreDraw(NPC npc, SpriteBatch spriteBatch, Vector2 screenPos, Color drawColor)
+        {
+            if (IsStunned)
+            {
+                // Increment frame
+                _stunFrameTimer++;
+                if (_stunFrameTimer > 6)
+                {
+                    _stunFrameTimer = 0;
+                    _stunFrame++;
+                    if (_stunFrame >= _frameCount - 1)
+                    {
+                        _stunFrame = 0;
+                    }
+                }
+
+                // Drawpos
+                Vector2 center = new Vector2(npc.Center.X, npc.position.Y - 10f);
+                Vector2 adjustedDrawPos = center - 0.5f * DRGTextures.StunTwinkle.Size();
+                int frameHeight = DRGTextures.StunTwinkle.Height / _frameCount;
+                Rectangle frame = new Rectangle(0, _stunFrame * frameHeight, DRGTextures.StunTwinkle.Width, frameHeight);
+                // Draw
+                Main.EntitySpriteDraw(new DrawData(DRGTextures.StunTwinkle, adjustedDrawPos - Main.screenPosition, frame, Color.White));
+            }
+
+            return base.PreDraw(npc, spriteBatch, screenPos, drawColor);
         }
     }
 }
