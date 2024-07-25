d65331d44e8f33616c14b7e966682bf15dc18ad4
scottsimply <53203532+scottysimply@users.noreply.github.com>
2024-07-25 15:44:56 -0500

    Plasma bullet is physically larger. May need an adjusted tile collide style.

--- Content/Projectiles/PlasmaProjectiles/PlasmaBullet.cs
+++ Content/Projectiles/PlasmaProjectiles/PlasmaBullet.cs
@@ -9,10 +9,14 @@ namespace deeprockitems.Content.Projectiles.PlasmaProjectiles
     {
         public override void SetDefaults()
         {
-            Projectile.width = 10;
-            Projectile.height = 10;
+            Projectile.width = 14;
+            Projectile.height = 14;
             Projectile.friendly = true;
             Projectile.hostile = false;
+        }
+        public override Color? GetAlpha(Color lightColor)
+        {
+            return Color.White;
         }
         public override string GlowTexture => "deeprockitems/Content/Projectiles/PlasmaProjectile/PlasmaBullet";
         public override bool PreDraw(ref Color lightColor)
