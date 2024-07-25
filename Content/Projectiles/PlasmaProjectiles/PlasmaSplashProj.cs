544506a1d3a634412d16c2642f9cd7629e00c718
scottsimply <53203532+scottysimply@users.noreply.github.com>
2024-07-25 15:46:21 -0500

    Implemented plasma splash projectile (smaller version of the plasma explosion)

---
+++ Content/Projectiles/PlasmaProjectiles/PlasmaSplashProj.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace deeprockitems.Content.Projectiles.PlasmaProjectiles
+{
+    public class PlasmaSplashProj : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.projFrames[Type] = 3;
+        }
+        public override void SetDefaults()
+        {
+            Projectile.width = Projectile.height = 60;
+            Projectile.frame = 0;
+            Projectile.tileCollide = false;
+            Projectile.friendly = true;
+            Projectile.hostile = false;
+            Projectile.penetrate = -1;
+        }
+        public override void AI()
+        {
+            Projectile.frameCounter++;
+            if (Projectile.frameCounter % 3 == 0 && Projectile.frame < 2)
+            {
+                Projectile.frame++;
+            }
+            if (Projectile.frameCounter > 10)
+            {
+                Projectile.Kill();
+            }
+        }
+        public override Color? GetAlpha(Color lightColor)
+        {
+            return Color.White;
+        }
+    }
+}
