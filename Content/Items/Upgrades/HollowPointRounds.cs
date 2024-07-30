73d64518a9cfba413938cf5a4dfd63f8c03199df
scottsimply <scottistriggered@gmail.com>
2024-07-30 10:03:26 -0500

    Added hollow point rounds

---
+++ Content/Items/Upgrades/HollowPointRounds.cs
@@ -0,0 +1,35 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using deeprockitems.Content.Items.Upgrades.PlasmaPistolUpgrades;
+using deeprockitems.Content.Items.Weapons;
+using System.Collections.Generic;
+using deeprockitems.Content.Buffs;
+
+namespace deeprockitems.Content.Items.Upgrades
+{
+    [ValidWeapons(
+        typeof(Zhukovs),
+        typeof(M1000),
+        typeof(JuryShotgun))]
+    public class HollowPointRounds : UpgradeTemplate
+    {
+        public override bool IsOverclock => false;
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            Item.rare = ItemRarityID.Orange;
+        }
+        public override void AddRecipes()
+        {
+
+        }
+        public class HollowPointProjectile : UpgradeGlobalProjectile<HollowPointRounds>
+        {
+            public override void UpgradeOnHitNPC(Projectile projectile, NPC target, NPC.HitInfo hit, int damageDone)
+            {
+                target.AddBuff(ModContent.BuffType<StunnedEnemy>(), 120);
+            }
+        }
+    }
+}
