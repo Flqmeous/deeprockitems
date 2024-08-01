0d1856628225ba776449b591e08b970757c3e371
scottsimply <scottistriggered@gmail.com>
2024-07-31 22:37:33 -0500

    Fixed typo

--- Content/Items/Upgrades/M1000Upgrades/SupercoolOC.cs
+++ Content/Items/Upgrades/M1000Upgrades/SupercoolOC.cs
@@ -21,7 +21,7 @@ namespace deeprockitems.Content.Items.Upgrades.M1000Upgrades
         {
             Recipe upgrade = Recipe.Create(ModContent.ItemType<SupercoolOC>())
             .AddIngredient<Misc.MatrixCore>()
-            .AddRecipeGroup(nameof(AdamantiteBar), 10)
+            .AddRecipeGroup(nameof(ItemID.AdamantiteBar), 10)
             .AddIngredient(ItemID.MusketBall, 75)
             .AddIngredient(ItemID.FrostCore)
             .AddTile(TileID.MythrilAnvil);
