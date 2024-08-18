7eb57718074749f416c62846fc7f990cca54b3e3
scottsimply <scottistriggered@gmail.com>
2024-08-17 19:30:22 -0500

    Added client config for mod

---
+++ Common/Config/DRGClientConfig.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ModLoader.Config;
+using Terraria.ModLoader.Config.UI;
+using deeprockitems.Common.Config.UIElements;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json;
+
+namespace deeprockitems.Common.Config
+{
+    public class DRGClientConfig : ModConfig
+    {
+        public override ConfigScope Mode => ConfigScope.ClientSide;
+
+
+        [Header("UI")]
+
+        [DefaultValue(TemperatureOptions.Vanilla)]
+        public TemperatureOptions temperatureDisplaySetting;
+    }
+    [CustomModConfigItem(typeof(SelectBoxParent<TemperatureOptions>))]
+    public enum TemperatureOptions
+    {
+        Vanilla = 0,
+        Fancy,
+    }
+}
