b27900d1b0047572586ce3129b9b4b044725ca1c
scottsimply <scottistriggered@gmail.com>
2024-09-03 16:59:30 -0500

    Added a global projectile for upgradable items. Hooks can be added or removed easily

---
+++ Content/Projectiles/UpgradeGlobalProjectile.cs
@@ -0,0 +1,57 @@
+﻿using deeprockitems.Content.Items.Weapons;
+using deeprockitems.Content.Upgrades;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ModLoader;
+
+namespace deeprockitems.Content.Projectiles
+{
+    public class UpgradeGlobalProjectile : GlobalProjectile
+    {
+        Upgrade[] _equippedUpgrades = [];
+        public override bool InstancePerEntity => true;
+        public override void OnSpawn(Projectile projectile, IEntitySource source)
+        {
+            if (source is not EntitySource_ItemUse newSource) return;
+
+            if (newSource.Item.ModItem is not UpgradeableItemTemplate modItem) return;
+
+            // Get list of equipped upgrades
+            _equippedUpgrades = GetEquippedUpgrades(modItem.UpgradeMasterList);
+
+            foreach (var upgrade in _equippedUpgrades)
+            {
+                upgrade.Projectile_OnSpawnHook?.Invoke(projectile, source);
+            }
+        }
+        public override void AI(Projectile projectile)
+        {
+            foreach (var upgrade in _equippedUpgrades)
+            {
+                upgrade.Projectile_AIHook?.Invoke(projectile);
+            }
+        }
+        public override void OnHitNPC(Projectile projectile, NPC target, NPC.HitInfo hit, int damageDone)
+        {
+            foreach (var upgrade in _equippedUpgrades)
+            {
+                upgrade.Projectile_OnHitNPCHook?.Invoke(projectile, target, hit, damageDone);
+            }
+        }
+        static Upgrade[] GetEquippedUpgrades(UpgradeList upgrades)
+        {
+            List<Upgrade> equippedUpgrades = new();
+            foreach (var tiers in upgrades)
+            {
+                foreach (var upgrade in tiers)
+                {
+                    if (!upgrade.IsEquipped) continue;
+
+                    equippedUpgrades.Add(upgrade);
+                }
+            }
+            return [.. equippedUpgrades];
+        }
+    }
+}
