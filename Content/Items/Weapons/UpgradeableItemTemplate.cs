0392dd753bfe16a6244a6bbad191b9cc7cd46cd6
scottsimply <scottistriggered@gmail.com>
2024-09-03 16:52:59 -0500

    Completely reintroduced upgrades as a concept onto the upgradeable items.

--- Content/Items/Weapons/UpgradeableItemTemplate.cs
+++ Content/Items/Weapons/UpgradeableItemTemplate.cs
@@ -1,4 +1,8 @@
-﻿using Terraria.GameContent.Creative;
+﻿using deeprockitems.Content.Upgrades;
+using deeprockitems.Types;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.GameContent.Creative;
 using Terraria.ModLoader;
 using Terraria.Utilities;
 
@@ -6,16 +10,41 @@ namespace deeprockitems.Content.Items.Weapons
 {
     public abstract class UpgradeableItemTemplate : ModItem
     {
-        public virtual void NewSetDefaults()
-        {
-            
-        }
+        public float UseTimeScale { get; set; }
+        public UpgradeList UpgradeMasterList { get; private set; }
+        public virtual void NewSetDefaults() { }
         public sealed override void SetDefaults()
         {
             NewSetDefaults();
             base.SetDefaults();
+            // Load upgrades and apply parent name to upgrade
+            UpgradeMasterList = InitializeUpgrades();
         }
-        public override void SetStaticDefaults()
+        public virtual void VerifyUpgrades()
+        {
+            ResetStats();
+            foreach (var upgradeTier in UpgradeMasterList)
+            {
+                foreach (var upgrade in upgradeTier)
+                {
+                    if (upgrade.IsEquipped)
+                    {
+                        upgrade.Item_ModifyStats?.Invoke();
+                    }
+                }
+            }
+        }
+        public override void Load()
+        {
+            _ = InitializeUpgrades();
+        }
+        public virtual void ResetStats() { }
+        /// <summary>
+        /// Initializes the master list of upgrades this weapon will use.
+        /// </summary>
+        /// <returns></returns>
+        public virtual UpgradeList InitializeUpgrades() => new("notSet");
+        public override sealed void SetStaticDefaults()
         {
             CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
             NewSetStaticDefaults();
