a396d57bf0f573fd42e1a70e50c8e5fb8239b123
scottsimply <scottistriggered@gmail.com>
2024-09-03 16:55:21 -0500

    I have not found a use for these types yet, however these are going to be very useful in terms of upgrades. Semi observable properties allow raising events when a property is changed, with discretion to what modifies it.

---
+++ Types/PropertyChangedArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace deeprockitems.Types
+{
+    public class PropertyChangedArgs<T>
+    {
+        public T OldValue { get; init; }
+        public T NewValue { get; init; }
+        public PropertyChangedArgs(T oldValue, T newValue)
+        {
+            OldValue = oldValue;
+            NewValue = newValue;
+        }
+    }
+}
