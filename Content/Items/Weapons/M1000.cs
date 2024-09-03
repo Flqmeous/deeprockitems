a1d0e6e3789e5ea012f5c23537661b7e0b7ad6da
scottsimply <scottistriggered@gmail.com>
2024-09-03 16:54:34 -0500

    Introduced a basic set of upgrades for the M1000. I'm going to go through the weapons in order of implementation and work my way through.

--- Content/Items/Weapons/M1000.cs
+++ Content/Items/Weapons/M1000.cs
@@ -1,5 +1,8 @@
-﻿using deeprockitems.Content.Projectiles;
+﻿using deeprockitems.Assets.Textures;
+using deeprockitems.Content.Buffs;
+using deeprockitems.Content.Projectiles;
 using deeprockitems.Content.Projectiles.M1000Projectile;
+using deeprockitems.Content.Upgrades;
 using deeprockitems.Utilities;
 using Microsoft.Xna.Framework;
 using Terraria;
@@ -31,6 +34,76 @@ namespace deeprockitems.Content.Items.Weapons
             Item.rare = ItemRarityID.Pink;
             Item.value = Item.sellPrice(0, 9, 25, 0);
             Item.consumable = false;
+        }
+        public override UpgradeList InitializeUpgrades()
+        {
+            return new UpgradeList("M1000",
+                new UpgradeTier(1,
+                    new Upgrade("DamageUpgrade", DRGTextures.DamageIcon) {
+                        Item_ModifyStats = () => {
+                            Item.damage = (int)(Item.OriginalDamage * 1.15f);
+                        }
+                    },
+                    new Upgrade("BumpFire", DRGTextures.DamageIcon) {
+                        Item_ModifyStats = () => {
+                            Item.useTime = Item.useAnimation = (int)(Item.useTime * 0.83f);
+                        }
+                    }
+                ),
+                new UpgradeTier(2,
+                    new Upgrade("QuickCharge", DRGTextures.DamageIcon) {
+                        Projectile_OnSpawnHook = (projectile, source) => {
+                            if (projectile.ModProjectile is not HeldProjectileBase modProj) return;
+
+                            modProj.ChargeTime = (int)(modProj.ChargeTime * 0.75f);
+                        }
+                    }
+                ),
+                new UpgradeTier(3,
+                    new Upgrade("FocusDamage", DRGTextures.DamageIcon) {
+                        Projectile_OnSpawnHook = (projectile, source) => {
+                            if (source is not EntitySource_FromHeldProjectile newSource) return;
+
+                            if (newSource.SourceProjectile.Projectile.timeLeft >= newSource.SourceProjectile.ProjectileTime) return;
+
+                            projectile.damage = (int)(projectile.damage * 1.5f);
+                        } 
+                    },
+                    new Upgrade("DamageUpgrade", DRGTextures.DamageIcon) {
+                        Item_ModifyStats = () => {
+                            Item.damage = (int)(Item.damage * 1.15f);
+                        }
+                    }
+                ),
+                new UpgradeTier(4,
+                    new Upgrade("Blowthrough", DRGTextures.DamageIcon) {
+                        Projectile_OnSpawnHook = (projectile, source) => {
+                            projectile.penetrate = projectile.maxPenetrate = 5;
+                        }
+                    },
+                    new Upgrade("", DRGTextures.DamageIcon) {
+                        Projectile_OnSpawnHook = (projectile, source) => {
+                            
+                        }
+                    }
+                ),
+                new UpgradeTier(5,
+                    new Upgrade("IncendiaryRounds", DRGTextures.DamageIcon) {
+                        Projectile_OnHitNPCHook = (projectile, target, hitInfo, damageDone) => {
+                            target.AddBuff(BuffID.OnFire3, 120);    
+                        }
+                    },
+                    new Upgrade("HollowPointRounds", DRGTextures.DamageIcon) {
+                        Projectile_OnHitNPCHook = (projectile, target, hitInfo, damageDone) => {
+                            target.AddBuff(ModContent.BuffType<StunnedEnemy>(), 120);
+                        }
+                    }
+                )
+            );
+        }
+        public override void ResetStats()
+        {
+            Item.damage = Item.OriginalDamage;
         }
         public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
         {
