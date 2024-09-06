292ca0e387f3ec43eca075cc2297003e4b7cfdfe
scottsimply <scottistriggered@gmail.com>
2024-09-06 10:21:10 -0500

    Resized upgrade menu

--- UI/UpgradeUI/UpgradeState.cs
+++ UI/UpgradeUI/UpgradeState.cs
@@ -11,9 +11,9 @@ namespace deeprockitems.UI.UpgradeUI
             Panel = new();
             // Set size and location
             Panel.Left.Pixels = 100;
-            Panel.Top.Pixels = 300;
+            Panel.Top.Pixels = 312;
             Panel.Height.Pixels = 300;
-            Panel.Width.Pixels = 800;
+            Panel.Width.Pixels = 600;
             Append(Panel);
         }
     }
