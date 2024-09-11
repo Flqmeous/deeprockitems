17af75ab9630d1eca629dc15bb64d84dd400a385
scottsimply <scottistriggered@gmail.com>
2024-09-10 20:40:57 -0500

    Added a new interface to handle upgradable anything. Right now it's _solely_ used for upgradableitem

---
+++ Content/Items/IUpgradable.cs
@@ -0,0 +1,37 @@
+﻿using deeprockitems.Content.Upgrades;
+using deeprockitems.Types;
+using System;
+using System;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.GameContent.Creative;
+using Terraria.ModLoader;
+using Terraria.Utilities;
+
+namespace deeprockitems.Content.Items
+{
+    public interface IUpgradable
+    {
+        public UpgradeList UpgradeMasterList { get; set; }
+        public virtual void VerifyUpgrades()
+        {
+            ResetStats();
+            foreach (var upgradeTier in UpgradeMasterList)
+            {
+                foreach (var upgrade in upgradeTier)
+                {
+                    if (upgrade.IsEquipped)
+                    {
+                        upgrade.Item_ModifyStats?.Invoke();
+                    }
+                }
+            }
+        }
+        public void ResetStats() { }
+        /// <summary>
+        /// Initializes the master list of upgrades this weapon will use.
+        /// </summary>
+        /// <returns></returns>
+        public UpgradeList InitializeUpgrades();
+    }
+}
