5bedae30127502f043bd674386bd08ac6ba92d3c
scottsimply <scottistriggered@gmail.com>
2024-09-10 20:44:07 -0500

    Creating a new damageclass for this mod. It's sad i know

---
+++ Common/DamageType/DRGDamageClass.cs
@@ -0,0 +1,18 @@
+﻿using deeprockitems.Content.Items.Weapons;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace deeprockitems.Common.DamageType
+{
+    public class DRGDamageClass : DamageClass
+    {
+        // These items will not benefit from any other modifiers or any damage variation
+        public override StatInheritanceData GetModifierInheritance(DamageClass damageClass) {
+            return new StatInheritanceData(0f, 0f, 0f, 0f, 0f);
+        }
+        public override bool GetEffectInheritance(DamageClass damageClass) {
+            return false;
+        }
+        public override bool UseStandardCritCalcs => false;
+    }
+}
