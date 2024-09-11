51360a1230ae0b050b5a67b2ffa594e116408c47
scottsimply <scottistriggered@gmail.com>
2024-09-10 20:43:37 -0500

    Quick fix for blowthrough on m1000. probably going away

--- Content/Projectiles/M1000Projectile/M1000Helper.cs
+++ Content/Projectiles/M1000Projectile/M1000Helper.cs
@@ -16,7 +16,11 @@ namespace deeprockitems.Content.Projectiles.M1000Projectile
         public override int ProjectileToSpawn { get; set; } = ProjectileID.BulletHighVelocity;
         public override SoundStyle? ChargeSound => DRGSoundIDs.M1000Focus;
         public override SoundStyle? FireSound => DRGSoundIDs.M1000Fire;
+        public override void ModifyProjectileAfterSpawning(Projectile projectile) {
+            if ((ammoUsed != ItemID.MusketBall || ammoUsed != ItemID.EndlessMusketPouch) && projectile.maxPenetrate == 5) return;
 
+            projectile.penetrate = 1;
+        }
         public override void WhenReachedFullCharge()
         {
             Projectile.damage *= 2;
