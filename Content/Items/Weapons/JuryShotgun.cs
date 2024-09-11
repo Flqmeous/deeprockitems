8161c7e1fe413195a37e0fc223bb07f72c0ce289
scottsimply <scottistriggered@gmail.com>
2024-09-10 20:42:51 -0500

    Added boomstick upgrades (3)

--- Content/Items/Weapons/JuryShotgun.cs
+++ Content/Items/Weapons/JuryShotgun.cs
@@ -7,19 +7,22 @@ using Terraria.DataStructures;
 using System.Collections.Generic;
 using Terraria.ModLoader.IO;
 using System;
+using deeprockitems.Content.Upgrades;
 
 namespace deeprockitems.Content.Items.Weapons
 {
-    public class JuryShotgun : UpgradeableItemTemplate
+    public class JuryShotgun : UpgradableWeapon
     {
         public override void NewSetDefaults()
         {
+            ResetStats();
             Item.CloneDefaults(ItemID.Boomstick);
             Item.damage = 15;
             Item.width = 40;
             Item.height = 16;
             Item.useTime = 45;
             Item.useAnimation = 45;
+            Item.autoReuse = true;
             Item.value = Item.sellPrice(0, 3, 0, 0);
         }
         /// <summary>
@@ -34,6 +37,33 @@ namespace deeprockitems.Content.Items.Weapons
         /// The lower bound of the shotgun velocity
         /// </summary>
         public float VelocityLowerBound { get; set; } = 0.8f;
+        public int PelletCount { get; set; } = 3;
+        public override UpgradeList InitializeUpgrades() {
+            return new UpgradeList("JuryShotgun",
+                new UpgradeTier(1,
+                    new Upgrade("MorePellets", Assets.Upgrades.Penetrate.Value) {
+                        Item_ModifyStats = () => {
+                            PelletCount += 2;
+                        }
+                    },
+                    new Upgrade("DamageUpgrade", Assets.Upgrades.Damage.Value) {
+                        Item_ModifyStats = () => {
+                            Item.damage += 2;
+                        }
+                    }
+                ),
+                new UpgradeTier(2, 
+                    new Upgrade("BiggerClip", Assets.Upgrades.FireRate.Value) {
+
+                    }
+                )
+            );
+        }
+        public override void ResetStats() {
+            COOLDOWN_THRESHOLD = 36f;
+            PelletCount = 3;
+            Item.damage = Item.OriginalDamage;
+        }
         public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
         {
             // Change player's direction to face the cursor
@@ -47,17 +77,9 @@ namespace deeprockitems.Content.Items.Weapons
             }
 
             // Shoot logic
-            int numberProjectiles = 3 + Main.rand.Next(1, 3);
+            int numberProjectiles = PelletCount + Main.rand.Next(1, 3);
             double spread = Math.PI / 13;
-            /*if (Upgrades.Contains(ModContent.ItemType<PelletAlignmentOC>())) // Reduced spread
-            {
-                spread *= .5;
-            }
-            else if (Upgrades.Contains(ModContent.ItemType<StuffedShellsOC>())) // twice amount of pellets, much more spread and lower firerate
-            {
-                numberProjectiles *= 2;
-                spread *= 2;
-            }*/
+
             // This block is for the projectile spread.
             int projectilesWithMultiplier = (int)Math.Floor(ProjectileMultiplier * numberProjectiles);
             for (int i = 0; i < projectilesWithMultiplier; i++)
