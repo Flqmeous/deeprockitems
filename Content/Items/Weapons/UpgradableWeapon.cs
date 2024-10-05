b064666d3dc8ecf540aa6cdf8c078c215b01cf1e
scottsimply <scottistriggered@gmail.com>
2024-10-05 16:33:25 -0500

    Cooldown will utilize a 0-100 scale. Upgrades are now serialized and saved when exiting world.

--- Content/Items/Weapons/UpgradableWeapon.cs
+++ Content/Items/Weapons/UpgradableWeapon.cs
@@ -1,12 +1,14 @@
-﻿using deeprockitems.Common.DamageType;
+﻿using deeprockitems.Common.EntitySources;
 using deeprockitems.Content.Upgrades;
-using deeprockitems.Types;
-using System;
-using System;
+using deeprockitems.UI.UpgradeUI;
+using Microsoft.Xna.Framework;
 using System.Collections.Generic;
+using System.Linq;
 using Terraria;
+using Terraria.DataStructures;
 using Terraria.GameContent.Creative;
 using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
 using Terraria.Utilities;
 
 namespace deeprockitems.Content.Items.Weapons
@@ -18,13 +20,21 @@ namespace deeprockitems.Content.Items.Weapons
         /// </summary>
         public bool IsWeaponEnabled { get; private set; } = true;
         /// <summary>
-        /// The time it takes for the weapon to be able to fire again after being disabled.
+        /// The threshold before the weapon is disabled.
         /// </summary>
-        public float COOLDOWN_THRESHOLD { get; set; } = 140f;
+        public const float COOLDOWN_THRESHOLD = 100f;
+        /// <summary>
+        /// How much cooldown is gained per shot. Defaults to 12f.
+        /// </summary>
+        public float ShotsUntilCooldown { get; set; } = 12f;
         /// <summary>
         /// The initial cooldown used to determine whether the weapon will go on cooldown
         /// </summary>
         public float Cooldown { get; set; } = 0;
+        /// <summary>
+        /// The time in ticks it takes for the weapon to cool down.
+        /// </summary>
+        public float CooldownTime { get; set; } = 2f;
         /// <summary>
         /// The timer used to passively cool down the weapon if it is not being used.
         /// </summary>
@@ -45,7 +55,7 @@ namespace deeprockitems.Content.Items.Weapons
             {
                 if (Cooldown > 0)
                 {
-                    Cooldown -= 0.4f;
+                    Cooldown -= (COOLDOWN_THRESHOLD / CooldownTime);
                 }
                 else
                 {
@@ -82,37 +92,86 @@ namespace deeprockitems.Content.Items.Weapons
             if (player.ItemAnimationActive && player.itemAnimation + 1 == Item.useAnimation)
             {
                 // Add to cooldown
-                Cooldown += 12;
+                Cooldown += COOLDOWN_THRESHOLD / ShotsUntilCooldown;
                 // Add to cooldown use timer
                 _cooldownTimer = 90;
             }
 
         }
+        #region Saving and loading upgrades
+        public override void SaveData(TagCompound tag) {
+            // Serialize each tag with the key being the upgrade name, and the data being the UpgradeState
+            foreach (var tier in UpgradeMasterList)
+            {
+                foreach (var upgrade in tier)
+                {
+                    // Save each upgrade state
+                    tag.Add($"{upgrade.InternalName}.{tier.Tier}.State", upgrade.UpgradeState);
+                }
+            }
+        }
+        public override void LoadData(TagCompound tag) {
+            // Set upgrades' states
+            foreach (var tier in UpgradeMasterList)
+            {
+                foreach (var upgrade in tier)
+                {
+                    // Early continue ensures that this upgrade must exist.
+                    if (!tag.ContainsKey($"{upgrade.InternalName}.{tier.Tier}.State")) continue;
+                    // If this upgrade name matches the key, then we can safely set that upgrade's state
+                    upgrade.UpgradeState = tag.Get<UpgradeStateBinding>($"{upgrade.InternalName}.{tier.Tier}.State");
+                }
+            }
+            // Apply stat upgrades
+            ApplyStatUpgrades();
+        }
+        #endregion
+        public override bool? CanAutoReuseItem(Player player) {
+            if (Item.channel && player.autoReuseAllWeapons) return true;
+            return null;
+        }
+        public override void ModifyTooltips(List<TooltipLine> tooltips) {
+            // Remove the crit line
+            tooltips.Find(tl => tl.FullName == "Terraria/CritChance")?.Hide();
+        }
         public override bool CanUseItem(Player player) {
             return IsWeaponEnabled;
         }
-        public float UseTimeScale { get; set; }
+        protected int _oldUseTime;
+        protected int _oldUseAnimation;
         public UpgradeList UpgradeMasterList { get; set; }
+        public Dictionary<string, UpgradeStateBinding> Upgrades = new();
         public virtual void NewSetDefaults() { }
         public sealed override void SetDefaults() {
+            ResetStats();
             NewSetDefaults();
-            Item.DamageType = ModContent.GetInstance<DRGDamageClass>();
+            _oldUseTime = Item.useTime;
+            _oldUseAnimation = Item.useAnimation;
             base.SetDefaults();
             // Load upgrades and apply parent name to upgrade
             UpgradeMasterList = InitializeUpgrades();
         }
-        public virtual void VerifyUpgrades() {
+        public virtual void ApplyStatUpgrades() {
+            // Reset global stats
+            Item.useTime = _oldUseTime;
+            Item.useAnimation = _oldUseAnimation;
             ResetStats();
             foreach (var upgradeTier in UpgradeMasterList)
             {
                 foreach (var upgrade in upgradeTier)
                 {
-                    if (upgrade.IsEquipped)
+                    if (upgrade.UpgradeState.IsEquipped)
                     {
-                        upgrade.Item_ModifyStats?.Invoke();
+                        upgrade.Behavior.Item_ModifyStats?.Invoke(Item);
                     }
                 }
             }
+        }
+        private Upgrade[] GetEquippedUpgrades() {
+            return (from upgradeTier in UpgradeMasterList
+                   from upgrade in upgradeTier
+                   where upgrade.UpgradeState.IsEquipped
+                   select upgrade).ToArray();
         }
         public override void Load() {
             _ = InitializeUpgrades();
@@ -128,6 +187,20 @@ namespace deeprockitems.Content.Items.Weapons
             NewSetStaticDefaults();
         }
         public virtual void NewSetStaticDefaults() { }
+        public sealed override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback) {
+            var newSource = new EntitySource_FromUpgradableWeapon(player, this, source.AmmoItemIdUsed, source.Context);
+            if (NewShoot(player, newSource, position, velocity, type, damage, knockback))
+            {
+                Projectile spawnedProj = Projectile.NewProjectileDirect(newSource, position, velocity, type, damage, knockback, owner: player.whoAmI);
+                // Activate upgrades if equipped
+                foreach (var upgrade in GetEquippedUpgrades())
+                {
+                    upgrade.Behavior.Item_OnShoot?.Invoke(Item, player, newSource, spawnedProj);
+                }
+            }
+            return false;
+        }
+        public virtual bool NewShoot(Player player, EntitySource_FromUpgradableWeapon source, Vector2 position, Vector2 velocity, int type, int damage, float knockback) => true;
         public override bool? PrefixChance(int pre, UnifiedRandom rand) {
             return false;
         }
