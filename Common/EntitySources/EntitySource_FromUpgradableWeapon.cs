a5e64b779ade6b216f61408374a0c1a46fb4c58e
scottsimply <scottistriggered@gmail.com>
2024-10-05 10:05:28 -0500

    Added two new entity sources which can be used to pass upgrades to any spawned projectiles.

---
+++ Common/EntitySources/EntitySource_FromUpgradableWeapon.cs
@@ -0,0 +1,20 @@
+﻿using deeprockitems.Content.Items.Weapons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.DataStructures;
+
+namespace deeprockitems.Common.EntitySources
+{
+    public class EntitySource_FromUpgradableWeapon : EntitySource_ItemUse_WithAmmo
+    {
+        public new UpgradableWeapon Item { get => _item; }
+        public UpgradableWeapon _item;
+        public EntitySource_FromUpgradableWeapon(Player player, UpgradableWeapon item, int ammoItemId, string context = null) : base(player, item.Item, ammoItemId, context) {
+            _item = item;
+        }
+    }
+}
---
+++ Common/EntitySources/EntitySource_FromUpgradableWeapon_HeldProjectile.cs
@@ -0,0 +1,12 @@
+﻿using deeprockitems.Content.Projectiles;
+using Terraria.DataStructures;
+using Terraria;
+using deeprockitems.Content.Items.Weapons;
+
+namespace deeprockitems.Common.EntitySources
+{
+    public class EntitySource_FromHeldProjectile(Player player, UpgradableWeapon item, int ammoItemID, HeldProjectileBase projectile, string context = null) : EntitySource_FromUpgradableWeapon(player, item, ammoItemID, context)
+    {
+        public HeldProjectileBase SourceProjectile { get => projectile; }
+    }
+}
