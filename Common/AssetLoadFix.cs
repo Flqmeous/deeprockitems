313038a76b263491305e56aad956ca7c36dfde20
scottsimply <scottistriggered@gmail.com>
2024-10-05 10:04:57 -0500

    Added a class to forceload some certain assets.

---
+++ Common/AssetLoadFix.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+
+namespace deeprockitems.Common
+{
+    /// <summary>
+    /// This class exists solely to load some important assets.
+    /// </summary>
+    public class AssetLoadFix : ModSystem
+    {
+        public override void SetStaticDefaults() {
+            _ = Assets.UI.SelectButton.Value;
+        }
+    }
+}
