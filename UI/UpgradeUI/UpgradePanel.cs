9b2650e0818d464bf42a8d65191230a51a244cb2
scottsimply <scottistriggered@gmail.com>
2024-10-05 17:12:31 -0500

    Added recipe behavior to the upgrade menu

--- UI/UpgradeUI/UpgradePanel.cs
+++ UI/UpgradeUI/UpgradePanel.cs
@@ -6,6 +6,14 @@ using deeprockitems.Content.Items.Weapons;
 using Terraria.GameContent.UI.Elements;
 using System;
 using Terraria.ModLoader.UI;
+using deeprockitems.Content.Upgrades;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Terraria.ID;
+using Terraria.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria.DataStructures;
 
 namespace deeprockitems.UI.UpgradeUI
 {
@@ -43,6 +51,7 @@ namespace deeprockitems.UI.UpgradeUI
             {
                 ScalePanel = true
             };
+            RecipeDisplay = new UpgradeRecipeDisplay();
 
 
             // Set size and position of parent slot
@@ -50,21 +59,58 @@ namespace deeprockitems.UI.UpgradeUI
             ParentSlot.Width.Pixels = ParentSlot.Height.Pixels = 52;
 
             // Set size and position of button
+            ForgeButton.Height = ParentSlot.Height;
             ForgeButton.HAlign = 1f;
             ForgeButton.OnLeftClick += ForgeButton_OnLeftClick;
 
             // Set size of upgrade panel
             UpgradeContainer = new(Width.Pixels, Height.Pixels - ParentSlot.Height.Pixels);
             UpgradeContainer.Top.Pixels = ParentSlot.Height.Pixels;
+            UpgradeContainer.OnLeftClick += UpgradeContainer_OnLeftClick;
 
             // Append everything
             Append(ParentSlot);
             Append(ForgeButton);
             Append(UpgradeContainer);
+
+            // Set recipe display position
+            RecipeDisplay.Top.Pixels = ParentSlot.Top.Pixels;
+            RecipeDisplay.Width.Pixels = Width.Pixels - ForgeButton.Width.Pixels - ParentSlot.Width.Pixels - 4 * PADDING;
+            RecipeDisplay.Left.Pixels = ParentSlot.Left.Pixels + ParentSlot.Width.Pixels + PADDING;
+            RecipeDisplay.Height.Pixels = ParentSlot.Height.Pixels;
+            RecipeDisplay.SetState(null);
+            Append(RecipeDisplay);
+        }
+        /// <summary>
+        /// Recipe display for whether an upgrade can be "bought" or not.
+        /// </summary>
+        public UpgradeRecipeDisplay RecipeDisplay { get; set; }
+        /// <summary>
+        /// Selects the locked upgrade for crafting. Will not attempt to unlock the upgrade; refer to <see cref="UpgradePanel.UnlockUpgrade()"/>
+        /// </summary>
+        private void SelectThisLockedUpgrade(UpgradeSelectOption option) {
+            RecipeDisplay.SetState(option);
         }
 
+        private void UpgradeContainer_OnLeftClick(UIMouseEvent evt, UIElement listeningElement) {
+            // Determine which upgrade was clicked:
+            if (evt.Target is not UpgradeSelectOption option) return;
+
+            if (!option.Upgrade.UpgradeState.IsUnlocked)
+            {
+                SelectThisLockedUpgrade(option);
+                return;
+            }
+
+            // Select this upgrade
+            option.SelectThisUpgrade();
+        }
         private void ParentSlot_OnItemSwap(Item itemNowInSlot, Item itemThatLeftSlot)
         {
+            // Remove currently selected locked upgrade
+            RecipeDisplay.SetState(null);
+
+            // Set upgrade state
             if (itemNowInSlot.ModItem is IUpgradable modItem)
             {
                 // Set upgrades
@@ -78,7 +124,75 @@ namespace deeprockitems.UI.UpgradeUI
 
         private void ForgeButton_OnLeftClick(UIMouseEvent evt, UIElement listeningElement)
         {
-            Main.NewText("Forging!");
+            // Check if a current recipe is put in
+            if (RecipeDisplay.Option is null || RecipeDisplay.Option.Upgrade is null) return;
+
+            // Check if the ingredients of the recipe are in the player's inventory
+            if (!TryToCraftItem(Main.LocalPlayer, RecipeDisplay.Option.Upgrade.Recipe)) return;
+
+            // Unlock the upgrade, equip it, and disable the recipe
+            RecipeDisplay.Option.Upgrade.UpgradeState.IsUnlocked = true;
+
+            // Select this upgrade through the recipe
+            RecipeDisplay.Option.SelectThisUpgrade();
+            // Set state to null
+            RecipeDisplay.SetState(null);
+            // Play sound to let the player know items were taken
+            SoundEngine.PlaySound(SoundID.Unlock);
+
+        }
+        /// <summary>
+        /// Attempt crafting the upgrade. Returns true if the item was successfully crafted.
+        /// </summary>
+        /// <param name="player"></param>
+        /// <param name="recipe"></param>
+        /// <returns></returns>
+        private bool TryToCraftItem(Player player, UpgradeRecipe recipe) {
+            List<Item> matchingItems = [];
+
+            // Iterate the player's inventory and find the max amount of items
+            for (int recipeIndex = 0; recipeIndex < recipe.Length; recipeIndex++)
+            {
+                for (int invIndex = 0; invIndex < 50; invIndex++)
+                {
+                    // If we didn't find an item, continue
+                    if (!recipe.ItemsAndAmounts[recipeIndex].AcceptedTypes.Contains(player.inventory[invIndex].type)) continue;
+
+                    // Put candidate item in the list
+                    matchingItems.Add(player.inventory[invIndex]);
+                }
+
+                // Sum stacks of the items. If we didn't find every item, then we are unable to craft the recipe.
+                int totalStack = matchingItems.Where(item => recipe.ItemsAndAmounts[recipeIndex].AcceptedTypes.Contains(item.type)).Sum(item => item.stack);
+                if (totalStack < recipe.ItemsAndAmounts[recipeIndex].Stack) return false;
+            }
+
+            // The recipe is craftable. Let's start crafting it.
+            for (int recipeIndex = 0; recipeIndex < recipe.Length; recipeIndex++)
+            {
+                // Take items required
+                int itemsRequired = recipe.ItemsAndAmounts[recipeIndex].Stack;
+                // Take items sequentially from inventory
+                foreach (var item in matchingItems)
+                {
+                    // Don't take items if the type is not a candidate.
+                    if (!recipe.ItemsAndAmounts[recipeIndex].AcceptedTypes.Contains(item.type)) continue;
+
+                    // Begin taking items
+                    int currentStack = 0;
+                    while (currentStack < itemsRequired)
+                    {
+                        item.stack--;
+                        currentStack++;
+                        if (item.stack == 0)
+                        {
+                            item.TurnToAir();
+                            item.maxStack = 0;
+                        }
+                    }
+                }
+            }
+            return true;
         }
     }
 }
---
+++ UI/UpgradeUI/UpgradeRecipeDisplay.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Terraria.UI;
+using Terraria.ID;
+using Terraria;
+using Microsoft.Xna.Framework;
+using ReLogic.Graphics;
+using Terraria.GameContent;
+using Terraria.Localization;
+using ReLogic.Content;
+using deeprockitems.Utilities;
+
+namespace deeprockitems.UI.UpgradeUI
+{
+    public class UpgradeRecipeDisplay : UIElement
+    {
+        public UpgradeSelectOption Option;
+        private UpgradeRecipeOption[] optionItems;
+        LocalizedText recipeText => Language.GetOrRegister("deeprockitems.Misc.UsefulWords.Recipe", () => "Recipe:");
+        public UpgradeRecipeDisplay() {
+            _ = recipeText;
+        }
+        public void SetState(UpgradeSelectOption option) {
+            SetPadding(6f);
+            Option = option;
+            // Ensure that no children exist
+            RemoveAllChildren();
+            optionItems = null;
+
+            if (option is null)
+            {
+                return;
+            }
+
+            optionItems = new UpgradeRecipeOption[option.Upgrade.Recipe.Length];
+            // Calculate available positions for children. Recipe items get aligned to the right
+            Rectangle dimensions = GetInnerDimensions().ToRectangle();
+            for (int i = optionItems.Length - 1; 0 <= i; i--)
+            {
+                int reverseIndex = optionItems.Length - 1 - i;
+                // Init
+                optionItems[reverseIndex] = new UpgradeRecipeOption((int)((Parent as UpgradePanel).ForgeButton.Height.Pixels * 0.8f), option.Upgrade.Recipe.ItemsAndAmounts[i]);
+                // Set positions
+                optionItems[reverseIndex].Left.Pixels = Width.Pixels - (reverseIndex + 1) * (optionItems[reverseIndex].Width.Pixels + 14f);
+                optionItems[reverseIndex].VAlign = 0.5f;
+                Append(optionItems[reverseIndex]);
+            }
+        }
+        public override void Draw(SpriteBatch spriteBatch) {
+            // Early return to ensure that a recipe is selected.
+            if (Option is null) return;
+
+            // Drawing logic
+            var dimensions = GetDimensions().ToRectangle();
+            var inner = GetDimensions().ToRectangle();
+            // Draw background panel
+            DRGHelpers.DrawPanel(spriteBatch, TextureAssets.InventoryBack9.Value, 8, 8, new Vector2(inner.X, inner.Y), inner.Width, inner.Height, Color.White);
+            int xOffset = 16;
+            // Calculate scale:
+            Asset<DynamicSpriteFont> font = FontAssets.MouseText;
+            Vector2 fontSize = font.Value.MeasureString(recipeText.Value);
+            float scale = dimensions.Height / fontSize.Y;
+            // Restart spritebatch with nearest neighbor to remove the terrible, horrifying, blur.
+            Utils.DrawBorderString(spriteBatch, recipeText.Value, new Vector2(dimensions.X + xOffset, dimensions.Y + (fontSize.Y * 0.25f)), Color.White, scale: scale);
+
+
+            // Draw children
+            foreach (var child in Children)
+            {
+                child.Draw(spriteBatch);
+            }
+        }
+    }
+}
