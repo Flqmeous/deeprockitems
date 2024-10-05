d4441048a047e163b0b7c9ab160d6fafb524dd7c
scottsimply <scottistriggered@gmail.com>
2024-10-05 16:12:39 -0500

    More loosely defined the interface to allow more control with upgradable content

--- Content/Items/IUpgradable.cs
+++ Content/Items/IUpgradable.cs
@@ -13,20 +13,7 @@ namespace deeprockitems.Content.Items
     public interface IUpgradable
     {
         public UpgradeList UpgradeMasterList { get; set; }
-        public virtual void VerifyUpgrades()
-        {
-            ResetStats();
-            foreach (var upgradeTier in UpgradeMasterList)
-            {
-                foreach (var upgrade in upgradeTier)
-                {
-                    if (upgrade.IsEquipped)
-                    {
-                        upgrade.Item_ModifyStats?.Invoke();
-                    }
-                }
-            }
-        }
+        public abstract void ApplyStatUpgrades();
         public void ResetStats() { }
         /// <summary>
         /// Initializes the master list of upgrades this weapon will use.
