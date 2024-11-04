82462aeb6e0c6a2535ab538e4bbcd87fd62b1244
scottsimply <scottistriggered@gmail.com>
2024-11-04 11:07:28 -0600

    Removed a debug output

--- Content/Buffs/Sludged.cs
+++ Content/Buffs/Sludged.cs
@@ -6,7 +6,6 @@ namespace deeprockitems.Content.Buffs
     public class Sludged : InstancedBuff
     {
         public override void UpdateLifeRegen(NPC npc, ref int damage) {
-            Main.NewText(TimeLeft);
             int dps = StrongSludge ? 30 : 15;
             npc.lifeRegen -= dps * 2;
             damage = dps;
