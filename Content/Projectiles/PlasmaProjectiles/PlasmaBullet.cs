8cf6523e39d7850843f1856df76ce2f818f875cd
scottsimply <scottistriggered@gmail.com>
2024-11-19 11:32:57 -0600

    Merged plasma splash with the regular plasma bullet

--- Content/Projectiles/PlasmaProjectiles/PlasmaBullet.cs
+++ Content/Projectiles/PlasmaProjectiles/PlasmaBullet.cs
@@ -7,12 +7,49 @@ namespace deeprockitems.Content.Projectiles.PlasmaProjectiles
 {
     public class PlasmaBullet : ModProjectile
     {
+        public override void SetStaticDefaults() {
+            Main.projFrames[Type] = 4;
+        }
         public override void SetDefaults()
         {
-            Projectile.width = 14;
-            Projectile.height = 14;
+            Projectile.width = 60;
+            Projectile.height = 60;
             Projectile.friendly = true;
+            Projectile.localNPCHitCooldown = -1;
+            Projectile.usesLocalNPCImmunity = true;
             Projectile.hostile = false;
+            Projectile.ai[0] = -1f;
+        }
+        public bool IsExploding { get => Projectile.ai[0] > 0f; set => Projectile.ai[0] = value ? 1f : -1f; }
+        public override void ModifyDamageHitbox(ref Rectangle hitbox) {
+            if (IsExploding)
+            {
+                return;
+            }
+            hitbox = new Rectangle(hitbox.X + 23, hitbox.Y + 23, 14, 14);
+        }
+        public void Explode() {
+            Projectile.Resize(60, 60);
+            IsExploding = true;
+            Projectile.frame = 1;
+            Projectile.timeLeft = 10;
+            Projectile.penetrate = -1;
+            Projectile.timeLeft = 11;
+            Projectile.tileCollide = false;
+            Projectile.velocity = Vector2.Zero;
+            //Projectile.Damage();
+        }
+        public override void AI() {
+            if (!IsExploding) return;
+            Projectile.frameCounter++;
+            if (Projectile.frameCounter % 3 == 0 && Projectile.frame < 2)
+            {
+                Projectile.frame++;
+            }
+            if (Projectile.frameCounter > 10)
+            {
+                Projectile.Kill();
+            }
         }
         public override Color? GetAlpha(Color lightColor)
         {
