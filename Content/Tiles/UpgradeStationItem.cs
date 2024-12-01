371b0691bcb744a08ee010bfbac82b7b72719ff1
scottsimply <scottistriggered@gmail.com>
2024-11-30 21:37:47 -0600

    Added recipe to the upgrade station

--- Content/Tiles/UpgradeStationItem.cs
+++ Content/Tiles/UpgradeStationItem.cs
@@ -1,4 +1,6 @@
-﻿using Terraria.ModLoader;
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
 
 namespace deeprockitems.Content.Tiles
 {
@@ -7,6 +9,13 @@ namespace deeprockitems.Content.Tiles
         public override void SetDefaults()
         {
             Item.DefaultToPlaceableTile(ModContent.TileType<UpgradeStation>());
+        }
+        public override void AddRecipes() {
+            Recipe.Create(Type)
+                .AddIngredient(ItemID.SunplateBlock, 20)
+                .AddRecipeGroup(nameof(ItemID.GoldBar), 8)
+                .AddIngredient(ItemID.Glass, 10)
+                .Register();
         }
     }
 }
