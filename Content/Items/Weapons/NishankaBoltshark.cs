f522c3c458c130bdfc5a771fb8c86c888e160453
Flqmeous <shrimpleshrimpleson@gmail.com>
2024-12-07 22:50:12 -0600

    Added Nishanka Boltshark

---
+++ Content/Items/Weapons/NishankaBoltshark.cs
@@ -0,0 +1,131 @@
+﻿using deeprockitems.Common.EntitySources;
+using deeprockitems.Content.Projectiles.NishankaBoltsharkProjectiles;
+using deeprockitems.Content.Upgrades;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace deeprockitems.Content.Items.Weapons {
+    public class NishankaBoltshark : UpgradableWeapon {
+        public int shotCounterMaximum = 5;
+        private int shotCounter = 0;
+        public int shotCounterCooldown = 90;
+        private int shotCounterTimer = 0;
+
+        public override void NewSetDefaults() {
+            Item.width = 52;
+            Item.height = 20;
+            Item.damage = 10;
+            Item.knockBack = 5f;
+            Item.noMelee = true;
+            Item.useAnimation = Item.useTime = 30;
+            Item.shootSpeed = 9f;
+            Item.useAmmo = AmmoID.Arrow;
+            Item.shoot = ProjectileID.PurificationPowder;
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.DamageType = DamageClass.Ranged;
+            Item.rare = ItemRarityID.Blue;
+            Item.autoReuse = true; // set to false and have true as an upgrade, this is just set as it is for now because it's nicer to my finger
+
+            this.ShotsUntilCooldown = 5f;
+            this.TimeToEndCooldown = 60;
+        }
+
+        public override void NewModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback, ref float spread) {
+            Vector2 offset = Vector2.Normalize(velocity) * 10f;
+            if (Collision.CanHit(position, 0, 0, position + offset, 0, 0)) {
+                position += offset;
+            }
+        }
+
+        public override bool NewShoot(Player player, EntitySource_FromUpgradableWeapon source, Vector2 position, Vector2 velocity, int type, int damage, float knockback) {
+            shotCounter = (shotCounter % shotCounterMaximum) + 1;
+            shotCounterTimer = 0;
+            if (shotCounter == shotCounterMaximum) {
+                velocity *= 2f;
+                damage += 5;
+                knockback += 2.0f;
+                Projectile.NewProjectile(source, position, velocity, ModContent.ProjectileType<Bolt>(), damage, knockback); // Fire custom bolt
+                SoundEngine.PlaySound(SoundID.Item11 with { Pitch = -0.6f }, position);
+                return false;
+            }
+            SoundEngine.PlaySound(SoundID.Item5, position);
+            return true;
+        }
+
+        /* Overwrites UpdateInventory in UpgradableWeapon.cs, so I'm writing it out for this commit. Would reset shotCounter after shotCounterCooldown ticks.
+        public override void UpdateInventory(Player player) {
+            if (shotCounter != 0) {
+                shotCounterTimer = (shotCounterTimer % shotCounterCooldown) + 1;
+                if (shotCounterTimer == shotCounterCooldown) {
+                    shotCounter = 0;
+                }
+            } else { shotCounterTimer = 0; }
+            
+        }*/
+
+        public override Vector2? HoldoutOffset() {
+            return new Vector2(-3f, 0f);
+        }
+
+        public override UpgradeList InitializeUpgrades() {
+            // upgrade ideas oops you didn't see this: alt shot but higher cooldown / 3rd shot but higher heat-up / no cooldown / auto-reuse
+            return new UpgradeList("NishankaBoltshark",
+                new UpgradeTier(1,
+                    new Upgrade("T1.1", Assets.Upgrades.Focus.Value) {
+                        Behavior = {
+                            /**/
+                        },
+                        Recipe = new UpgradeRecipe()
+                               .AddCandidateIngredient([ItemID.AaronsBreastplate, ItemID.ZombieMermanBanner], 1)
+                    }
+                ),
+                new UpgradeTier(2,
+                    new Upgrade("T2.1", Assets.Upgrades.TemperatureDecrease.Value) {
+                        Behavior = {
+                            /**/
+                        },
+                        Recipe = new UpgradeRecipe()
+                                .AddCandidateIngredient([ItemID.AaronsBreastplate, ItemID.ZombieMermanBanner], 1)
+                    }
+                ),
+                new UpgradeTier(3,
+                    new Upgrade("T3.1", Assets.Upgrades.TemperatureDecrease.Value) {
+                        Behavior = {
+                            /**/
+                        },
+                        Recipe = new UpgradeRecipe()
+                                .AddCandidateIngredient([ItemID.AaronsBreastplate, ItemID.ZombieMermanBanner], 1)
+                    }
+                ),
+                new UpgradeTier(4,
+                    new Upgrade("T4.1", Assets.Upgrades.TemperatureDecrease.Value) {
+                        Behavior = {
+                            /**/
+                        },
+                        Recipe = new UpgradeRecipe()
+                                .AddCandidateIngredient([ItemID.AaronsBreastplate, ItemID.ZombieMermanBanner], 1)
+                    }
+                ),
+                new UpgradeTier(5,
+                    new Upgrade("T5.1", Assets.Upgrades.TemperatureDecrease.Value) {
+                        Behavior = {
+                            /**/
+                        },
+                        Recipe = new UpgradeRecipe()
+                                .AddCandidateIngredient([ItemID.AaronsBreastplate, ItemID.ZombieMermanBanner], 1)
+                    }
+                )
+            );
+
+        }
+
+    }
+}
---
+++ Content/Projectiles/NishankaBoltsharkProjectiles/Bolt.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace deeprockitems.Content.Projectiles.NishankaBoltsharkProjectiles {
+    public class Bolt : ModProjectile {
+        public override void SetDefaults() {
+            Projectile.height = 10;
+            Projectile.width = 10;
+
+            Projectile.damage = 10;
+            Projectile.penetrate = 1;
+
+            Projectile.friendly = true;
+            Projectile.arrow = true;
+            
+            Projectile.DamageType = DamageClass.Ranged;
+
+            Projectile.usesLocalNPCImmunity = true;
+            Projectile.localNPCHitCooldown = 30;
+        }
+
+        public override void AI() {
+            Projectile.ai[0] += 1f;
+            if (Projectile.ai[0] >= 15f) {
+                Projectile.ai[0] = 15f;
+                Projectile.velocity.Y += 0.1f;
+            }
+
+            Projectile.rotation = Projectile.velocity.ToRotation() + MathHelper.PiOver2;
+
+            if (Projectile.velocity.Y > 16f) {
+                Projectile.velocity.Y = 16f;
+            }
+        }
+
+        public override void OnKill(int timeLeft) {
+            SoundEngine.PlaySound(SoundID.Dig, Projectile.position);
+            for (int i = 0; i < 5; i++) {
+                Dust dust = Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, DustID.Dirt);
+                dust.noGravity = true;
+                dust.velocity *= 1.5f;
+                dust.scale *= 0.9f;
+            }
+        }
+    }
+}
